3cc153fddcd3a7676872937654a24478d7067004
Dennmed22 <medrano.dennis@yahoo.com>
2023-08-03 19:32:44 -0400

    added webpack info and json files

--- Main/client/webpack.config.js
+++ Main/client/webpack.config.js
@@ -3,13 +3,16 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
@@ -18,12 +21,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Enables note-taking with JavaScript syntax!',
+        background_color: '#ffa07a',
+        theme_color: '#ffa07a',
+        start_url: '/',
+        publicPath: '/',
+      }),
     ],
 
     module: {
+      // CSS-Loader
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader in used with ES6 to load.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
